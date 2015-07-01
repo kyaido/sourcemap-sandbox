f08a5692fb982b2e44b50f1ca61ab25ad9002482
kaido <kaido@team-lab.com>
2015-07-01 15:40:24 +0900

    NG commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps    = require('gulp-sourcemaps');
 
 var AUTOPREFIXER_BROWSERS = [
   'last 2 version', 'Explorer >= 8', 'Android >= 2.3'
@@ -9,9 +10,13 @@ var AUTOPREFIXER_BROWSERS = [
 /* sass task */
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     }))
+    .pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src/scss'}))
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css/'));
 });
