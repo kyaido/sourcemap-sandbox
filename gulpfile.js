198c2ffaf9844773a130119187e25ddebc7d5bdb
kyaido <kamehameha1985@gmail.com>
2015-07-07 12:14:36 +0900

    commit sample files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps    = require('gulp-sourcemaps');
-
-var AUTOPREFIXER_BROWSERS = [
-  'last 2 version', 'Explorer >= 8', 'Android >= 2.3'
-];
+var sourcemaps   = require('gulp-sourcemaps');
 
 /* sass task */
 gulp.task('sass', function() {
@@ -14,9 +9,6 @@ gulp.task('sass', function() {
     .pipe(sass({
       outputStyle: 'compressed'
     }))
-    // .pipe(sourcemaps.write({includeContent: false, sourceRoot: '../../src/scss'}))
-    // .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
     .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src/scss'}))
     .pipe(gulp.dest('dist/css/'));
 });
