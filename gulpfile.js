54df8106db9da2b55894c8faba8e23249794def2
kyaido <kamehameha1985@gmail.com>
2015-07-15 13:29:54 +0900

    commit test files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,49 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps   = require('gulp-sourcemaps');
+var minifyCss    = require('gulp-minify-css');
+var plumber      = require('gulp-plumber');
+var webserver    = require('gulp-webserver');
 
 var AUTOPREFIXER_BROWSERS = [
-  'last 2 version', 'Explorer >= 8', 'Android >= 2.3'
+  'last 2 version', 'Explorer >= 9', 'Android >= 4.0'
 ];
 
 /* sass task */
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({
-      outputStyle: 'compressed'
+      errLogToConsole: true,
+      // outputStyle: 'compressed'
     }))
-    // .pipe(sourcemaps.write({includeContent: false, sourceRoot: '../../src/scss'}))
-    // .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src/scss'}))
+    .pipe(minifyCss({advanced: false}))
+    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(sourcemaps.write('maps/'))
     .pipe(gulp.dest('dist/css/'));
 });
+
+
+/* webserver task */
+gulp.task('webserver', function() {
+  gulp.src('./dist/')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: 8000,
+      livereload: true,
+      directoryListing: false,
+      open: 'http://localhost:8000'
+    }));
+});
+
+
+/* watch task */
+gulp.task('watch', function() {
+  gulp.watch(['src/scss/**/*.scss'], ['sass']);
+});
+
+
+/* default task */
+gulp.task('default', ['webserver', 'watch']);
