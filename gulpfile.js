53e9ede52b1f6a9d28c6bf5996fc82710c3f0662
kyaido <kamehameha1985@gmail.com>
2015-07-16 11:24:16 +0900

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,16 @@
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-
-var AUTOPREFIXER_BROWSERS = [
-  'last 2 version', 'Explorer >= 8', 'Android >= 2.3'
-];
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var minifyCss  = require('gulp-minify-css');
 
 /* sass task */
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
-      outputStyle: 'compressed'
+      // outputStyle: 'compressed'
     }))
-    // .pipe(sourcemaps.write({includeContent: false, sourceRoot: '../../src/scss'}))
-    // .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src/scss'}))
+    .pipe(minifyCss({advanced:false}))
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('dist/css/'));
 });
